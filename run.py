80922641e31db4caa8deeee18fe43ee6a46a55a3
Greg Payne <gregpayne@microsoft.com>
2023-11-26 18:46:31 +0000

    Comment out local files paths

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ app = create_app(debug=True)
 
 if __name__ == '__main__':
    # Run from the same directory as this script
-   this_files_dir = os.path.dirname(os.path.abspath(__file__))
-   os.chdir(this_files_dir)
+   # this_files_dir = os.path.dirname(os.path.abspath(__file__))
+   # os.chdir(this_files_dir)
 
    # host = socket.gethostbyname(socket.gethostname()) # by getting the IP address of the host, this can be run on multiple PC's without changing the address
    # port = 8003
