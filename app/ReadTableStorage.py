45c5b756f44cf1cd38a1b08a5b5e0fc89b9398d1
Greg Payne <gregpayne@microsoft.com>
2023-11-26 14:38:33 +0000

    Reading data into a dataframe

--- app/ReadTableStorage.py
+++ app/ReadTableStorage.py
@@ -1,13 +1,23 @@
 from azure.data.tables import TableClient
 import os
+import pandas as pd
 
 TABLE_CONNECTION_STRING = os.environ["TABLE_CONNECTION_STRING"]
 
+def convertTime(datetime):
+    return datetime.replace("T", " ").replace("Z", "")
+
 if __name__ == "__main__":
+    dateFormatter = '%Y-%m-%dT%H:%M:%S.%fZ'
     service = TableClient.from_connection_string(conn_str=TABLE_CONNECTION_STRING, table_name='greenhouseenvironment')
     filter = "PartitionKey eq '1'"
-    entries = service.query_entities(query_filter=filter)
+    entries = service.query_entities(query_filter=filter, headers={'Accept': 'application/json;odata=nometadata'})
+    data = []
     for entry in entries:
-        record = {"EventEnqueuedUtcTime": entry["EventEnqueuedUtcTime"], "Temperature": entry["temperature"]}
-        print(record)
-
+        # print(entry)
+        record = {"dateTime": convertTime(entry['EventEnqueuedUtcTime']), 'temperature': entry['temperature'], 'humidity': entry['humidity']}
+        data.append(record)
+    # print(data)
+    df = pd.DataFrame(data)
+    print(df)
+    
